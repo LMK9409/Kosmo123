305d11c2fe3f01f0b3773f3cbc43f88249fbde35
kosmo-20 <kosmo-20@DESKTOP-Q228BGD>
2018-12-15 19:31:35 +0900

    LMK

--- Webteam_prj/src/com/jang/order/OrderListServlet.java
+++ Webteam_prj/src/com/jang/order/OrderListServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class OrderListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
+    /** 
      * @see HttpServlet#HttpServlet()
      */
 
