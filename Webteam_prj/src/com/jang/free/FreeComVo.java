336a6ea911d0396e6b520ad7e1ba5e7caf090a82
kosmo-09 <kosmo-09@DESKTOP-G9K9LJ6>
2018-12-17 20:58:09 +0900

    자게 패키지업뎃

---
+++ Webteam_prj/src/com/jang/free/FreeComVo.java
@@ -0,0 +1,40 @@
+package com.jang.free;
+
+public class FreeComVo {
+	private int rSeq;
+	private String rText;
+	private int mSeq;
+	private int fSeq;
+	private String rRegdate;
+	public int getrSeq() {
+		return rSeq;
+	}
+	public void setrSeq(int rSeq) {
+		this.rSeq = rSeq;
+	}
+	public String getrText() {
+		return rText;
+	}
+	public void setrText(String rText) {
+		this.rText = rText;
+	}
+	public int getmSeq() {
+		return mSeq;
+	}
+	public void setmSeq(int mSeq) {
+		this.mSeq = mSeq;
+	}
+	public int getfSeq() {
+		return fSeq;
+	}
+	public void setfSeq(int fSeq) {
+		this.fSeq = fSeq;
+	}
+	public String getrRegdate() {
+		return rRegdate;
+	}
+	public void setrRegdate(String rRegdate) {
+		this.rRegdate = rRegdate;
+	}
+	
+}
---
+++ Webteam_prj/src/com/jang/free/FreeDAO.java
@@ -0,0 +1,34 @@
+package com.jang.free;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.jang.common.MyBatisFactory;
+import com.jang.order.orderVO;
+
+public class FreeDAO {
+	public ArrayList<FreeVO> selectAll(){
+		SqlSession conn=null;
+		ArrayList<FreeVO> resList = null;
+		try {
+			conn=MyBatisFactory.getFactory().openSession(); 
+			resList=(ArrayList)conn.selectList("freeNameSpace.freeAll");
+		}catch(Exception e) {
+			conn.close();
+		}
+		return resList;
+	}
+	public  FreeVO selectOne(int fseq) {
+	      SqlSession conn =null;
+	      FreeVO res =null;
+	      try {
+	         conn = MyBatisFactory.getFactory().openSession(); 
+	         res =(FreeVO)conn.selectOne("freeNameSpace.freeOne",fseq);   
+	      }catch(Exception e) {
+	    	  conn.close();
+	      }
+	      return res;
+	   }
+	
+}
